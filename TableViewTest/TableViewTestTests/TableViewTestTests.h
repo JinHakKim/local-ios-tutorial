d8117703e9a062cff87104926c4f93b1586f141c
JinHakKim <uyrp123@nate.com>
2012-02-15 13:07:52 +0900

    create tableview project

---
+++ TableViewTest/TableViewTestTests/TableViewTestTests.h
@@ -0,0 +1,13 @@
+//
+//  TableViewTestTests.h
+//  TableViewTestTests
+//
+//  Created by Kim jinhak on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TableViewTestTests : SenTestCase
+
+@end
---
+++ TableViewTest/TableViewTestTests/TableViewTestTests.m
@@ -0,0 +1,32 @@
+//
+//  TableViewTestTests.m
+//  TableViewTestTests
+//
+//  Created by Kim jinhak on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TableViewTestTests.h"
+
+@implementation TableViewTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TableViewTestTests");
+}
+
+@end
